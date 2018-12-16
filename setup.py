5c639c903c43188de65be4757c75830febf05fc1
David Gadling <dave@toasterwaffles.com>
2018-12-15 16:13:27 -0800

    basics - requirements & setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Hyperion",
+    version="0.1",
+    author="Dave Gadling",
+    author_email="dave@toasterwaffles.com",
+    description="Watches Spartan for new races / race changes",
+    license="MIT",
+    install_requires=["requests"]
+)
