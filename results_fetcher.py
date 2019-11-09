8ecd3154a944fc4d9ed9b62ec3698b81ff549205
David Gadling <dave@toasterwaffles.com>
2019-11-09 08:16:17 -0800

    [results fetcher] Various perf/prod improvements
    1. Sleep longer between fetches, to be nicer.
    2. Read from JSON file, not hard-coded list
    3. Write to year directories
    4. Write files based on IDs, not human generated names

--- results_fetcher.py
+++ results_fetcher.py
@@ -96,10 +96,11 @@ def get_results(
     race_id: int,
     bracket_id: int,
     batch_size=500,
-    min_sleep=0.0,
-    max_sleep=1.0,
+    min_sleep=0.03,
+    max_sleep=3.09,
 ) -> List[RacerResult]:
     meta = get_metadata(event_id, race_id, bracket_id)
+    logging.info(f"  Have {meta['total_results']:,} results to fetch")
     results: List[RacerResult] = []
 
     start_positions = range(0, meta["total_results"], batch_size)
@@ -112,366 +113,54 @@ def get_results(
     return results
 
 
-races = [
-    {
-        "year": 2019,
-        "venue": "lebec",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "elite",
-        "event": 46713,
-        "race": 118132,
-        "bracket": 1285188,
-    },
-    {
-        "year": 2019,
-        "venue": "lebec",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "ag",
-        "event": 46713,
-        "race": 118133,
-        "bracket": 1285191,
-    },
-    {
-        "year": 2019,
-        "venue": "lebec",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "open",
-        "event": 46713,
-        "race": 118134,
-        "bracket": 1285206,
-    },
-    {
-        "year": 2019,
-        "venue": "seattle",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "elite",
-        "event": 43572,
-        "race": 109482,
-        "bracket": 1188538,
-    },
-    {
-        "year": 2019,
-        "venue": "seattle",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "ag",
-        "event": 43572,
-        "race": 109483,
-        "bracket": 1188541,
-    },
-    {
-        "year": 2019,
-        "venue": "seattle",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "open",
-        "event": 43572,
-        "race": 109484,
-        "bracket": 1188556,
-    },
-    {
-        "year": 2019,
-        "venue": "hawaii",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "elite",
-        "event": 45512,
-        "race": 114773,
-        "bracket": 1250136,
-    },
-    {
-        "year": 2019,
-        "venue": "hawaii",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "ag",
-        "event": 45512,
-        "race": 114774,
-        "bracket": 1250139,
-    },
-    {
-        "year": 2019,
-        "venue": "hawaii",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "heat": "open",
-        "event": 45512,
-        "race": 114775,
-        "bracket": 1250154,
-    },
-    {
-        "year": 2019,
-        "venue": "monterey",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 41665,
-        "heat": "elite",
-        "race": 104416,
-        "bracket": 1132032,
-    },
-    {
-        "year": 2019,
-        "venue": "monterey",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 41665,
-        "heat": "ag",
-        "race": 104417,
-        "bracket": 1132035,
-    },
-    {
-        "year": 2019,
-        "venue": "monterey",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 41665,
-        "heat": "open",
-        "race": 104418,
-        "bracket": 1132038,
-    },
-    {
-        "year": 2019,
-        "venue": "big-bear",
-        "day": "sunday",
-        "time": "morning",
-        "kind": "sprint",
-        "event": 43269,
-        "heat": "elite",
-        "race": 108532,
-        "bracket": 1178600,
-    },
-    {
-        "year": 2019,
-        "venue": "big-bear",
-        "day": "sunday",
-        "time": "morning",
-        "kind": "sprint",
-        "event": 43269,
-        "heat": "ag",
-        "race": 108533,
-        "bracket": 1178603,
-    },
-    {
-        "year": 2019,
-        "venue": "big-bear",
-        "day": "sunday",
-        "time": "morning",
-        "kind": "sprint",
-        "event": 43269,
-        "heat": "open",
-        "race": 108534,
-        "bracket": 1178618,
-    },
-    {
-        "year": 2019,
-        "venue": "san-jose",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 38970,
-        "heat": "elite",
-        "race": 97603,
-        "bracket": 1054059,
-    },
-    {
-        "year": 2019,
-        "venue": "san-jose",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 38970,
-        "heat": "ag",
-        "race": 97604,
-        "bracket": 1054062,
-    },
-    {
-        "year": 2019,
-        "venue": "san-jose",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 38970,
-        "heat": "open",
-        "race": 97605,
-        "bracket": 1054065,
-    },
-    {
-        "year": 2018,
-        "venue": "att-park",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "stadium-sprint",
-        "event": 36583,
-        "heat": "elite",
-        "race": 91892,
-        "bracket": 997205,
-    },
-    {
-        "year": 2018,
-        "venue": "att-park",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "stadium-sprint",
-        "event": 36583,
-        "heat": "ag",
-        "race": 91893,
-        "bracket": 997212,
-    },
-    {
-        "year": 2018,
-        "venue": "att-park",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "stadium-sprint",
-        "event": 36583,
-        "heat": "open",
-        "race": 91894,
-        "bracket": 997243,
-    },
-    {
-        "year": 2018,
-        "venue": "lebec",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "event": 36347,
-        "heat": "elite",
-        "race": 91272,
-        "bracket": 991307,
-    },
-    {
-        "year": 2018,
-        "venue": "lebec",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "event": 36347,
-        "heat": "ag",
-        "race": 91273,
-        "bracket": 991312,
-    },
-    {
-        "year": 2018,
-        "venue": "lebec",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "beast",
-        "event": 36347,
-        "heat": "open",
-        "race": 91274,
-        "bracket": 991343,
-    },
-    {
-        "year": 2018,
-        "venue": "monterey",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 30838,
-        "heat": "elite",
-        "race": 76842,
-        "bracket": 821687,
-    },
-    {
-        "year": 2018,
-        "venue": "monterey",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 30838,
-        "heat": "ag",
-        "race": 76843,
-        "bracket": 821690,
-    },
-    {
-        "year": 2018,
-        "venue": "monterey",
-        "day": "saturday",
-        "time": "morning",
-        "kind": "super",
-        "event": 30838,
-        "heat": "open",
-        "race": 76844,
-        "bracket": 821693,
-    },
-    {
-        "year": 2018,
-        "venue": "san-jose",
-        "day": "sunday",
-        "time": "morning",
-        "kind": "sprint",
-        "event": 29145,
-        "heat": "elite",
-        "race": 72646,
-        "bracket": 769369,
-    },
-    {
-        "year": 2018,
-        "venue": "san-jose",
-        "day": "sunday",
-        "time": "morning",
-        "kind": "sprint",
-        "event": 29145,
-        "heat": "ag",
-        "race": 72647,
-        "bracket": 769372,
-    },
-    {
-        "year": 2018,
-        "venue": "san-jose",
-        "day": "sunday",
-        "time": "morning",
-        "kind": "sprint",
-        "event": 29145,
-        "heat": "open",
-        "race": 72648,
-        "bracket": 769375,
-    },
-    # {"year": , "venue": "", "day": "", "time": "", "kind": "", "event": , "heat": "elite", "race": , "bracket": },
-    # {"year": , "venue": "", "day": "", "time": "", "kind": "", "event": , "heat": "ag",    "race": , "bracket": },
-    # {"year": , "venue": "", "day": "", "time": "", "kind": "", "event": , "heat": "open",  "race": , "bracket": },
-]
-
-
 def main():
-    filename_format = "{year}-{venue}-{day}-{time}-{heat}.csv"
+    filename_format = "{event_id}-{race_id}-{bracket_id}.csv"
+    output_dir = "race-results"
+    skipped_events = []
 
-    for race_info in races:
-        filename = os.path.join("race-results", filename_format.format(**race_info))
-        if os.path.exists(filename):
-            logging.info(f"{filename} exists, skipping!")
+    with open("interesting_events.json", "r", newline="") as in_f:
+        interesting_events = json.load(in_f)
+
+    for event_info in interesting_events:
+        if not isinstance(event_info["year"], int):
+            logging.warning(
+                f"{event_info['event']} has non-int year {event_info['year']}"
+            )
+            skipped_events.append(event_info)
             continue
 
-        results = get_results(
-            race_info["event"], race_info["race"], race_info["bracket"]
-        )
+        year_dir = os.path.join(output_dir, str(event_info["year"]))
+        if not os.path.exists(year_dir):
+            logging.info(f"Creating {year_dir}")
+            os.mkdir(year_dir)
 
-        logging.info(f"Working on {filename}")
-        with open(filename, "w", encoding="utf-8", newline="") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=RacerResult.columns())
-            writer.writeheader()
-            for row in results:
-                try:
-                    writer.writerow(row.as_dict())
-                except Exception:
-                    logging.exception(f"Issue with {row}")
-                    raise
+        logging.info(f"Looking at {event_info['year']} {event_info['name']}")
+        for race_info in event_info["races"]:
+            filename = os.path.join(year_dir, filename_format.format(**race_info))
+            if os.path.exists(filename):
+                logging.info(f"  {filename} exists, skipping!")
+                continue
+
+            try:
+                results = get_results(
+                    race_info["event_id"], race_info["race_id"], race_info["bracket_id"]
+                )
+            except Exception:
+                logging.exception(
+                    f"  Skipping {race_info['race_id']}|{race_info['bracket_id']}"
+                )
+                continue
+
+            logging.info(f"  Writing to {filename}")
+            with open(filename, "w", encoding="utf-8", newline="") as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=RacerResult.columns())
+                writer.writeheader()
+                for row in results:
+                    try:
+                        writer.writerow(row.as_dict())
+                    except Exception:
+                        logging.exception(f"Issue with {row}")
+                        raise
         logging.info("Done!")
     logging.info("Done with all races!")
 
