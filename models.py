8295a54b26d705b8ba78b557324df6b210af0385
David Gadling <dave@toasterwaffles.com>
2022-12-18 13:31:16 -0800

    [models] Add fields to Race
    Add some extended location information to the Race object.
    
    Refactor initialization into it's own function.
    
    Create a first migration function.

--- models.py
+++ models.py
@@ -2,6 +2,8 @@
 
 
 from datetime import date
+
+import playhouse.migrate as migrate
 from peewee import *
 
 db = SqliteDatabase("races.db")
@@ -32,6 +34,11 @@ class Race(BaseModel):
     spartan_id = IntegerField()
     name = CharField()
     start_date = DateField()
+    venue_name = CharField()
+    country = CharField()
+    region = CharField()
+    latitude = DoubleField()
+    longitude = DoubleField()
 
 
 class Event(BaseModel):
@@ -44,7 +51,28 @@ class Event(BaseModel):
     venue_name = CharField()
 
 
-if __name__ == "__main__":
+def init_db():
     db.connect()
     db.create_tables([Event, Race])
     db.close()
+
+
+def extended_location_migration():
+    migrator = migrate.SqliteMigrator(db)
+    venue_name = CharField(default='TBD')
+    country = CharField(default='TBD')
+    region = CharField(default='TBD')
+    latitude = DoubleField(default=0.0)
+    longitude = DoubleField(default=0.0)
+    with db.atomic():
+        migrate.migrate(
+            migrator.add_column('race', 'venue_name', venue_name),
+            migrator.add_column('race', 'country', country),
+            migrator.add_column('race', 'region', region),
+            migrator.add_column('race', 'latitude', latitude),
+            migrator.add_column('race', 'longitude', longitude),
+        )
+
+
+if __name__ == "__main__":
+    print("Update me with what you want to do!")
