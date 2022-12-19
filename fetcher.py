315893af22cdfe3c505f556db81b9c004678ffa0
David Gadling <dave@toasterwaffles.com>
2022-12-18 20:15:55 -0800

    [fetcher] Record event location information
    1. Black format
    2. Only warning's out of peewee
    3. Record venue location to the Race object

--- fetcher.py
+++ fetcher.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from datetime import date, datetime
-import logging
 import json
+import logging
 import os
+from datetime import date, datetime
 from typing import List
 
 import requests
@@ -48,7 +48,7 @@ def fetch_raw_race_info(persist: bool = False, file_name: str = None) -> List:
     headers = {
         "referrer": "https://www.spartan.com/en/race/find-race",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-        " (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+                      " (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
     }
 
     r = requests.get(
@@ -74,24 +74,31 @@ def fetch_raw_race_info(persist: bool = False, file_name: str = None) -> List:
 def main():
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(module)s: %(message)s",
-        level=logging.ERROR,
+        level=logging.INFO,
     )
 
     logging.getLogger("chardet").setLevel(logging.WARNING)
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("peewee").setLevel(logging.WARNING)
 
     logging.debug("Fetching data from Spartan")
     info = fetch_raw_race_info(persist=False, file_name="race_info.json")
     logging.debug("Time to compare what we found!")
 
     for overall_event in info:
+        venue_info = overall_event["venue"]
         curr_race = Race(
             spartan_id=overall_event["id"],
             name=overall_event["name"],
             start_date=datetime.strptime(
                 overall_event["start_date"], "%Y-%m-%d"
             ).date(),
+            venue_name=venue_info["name"],
+            country=venue_info.get("country", "TBD"),
+            region=venue_info.get("region", "TBD"),
+            latitude=float(venue_info.get("latitude", "0.0")),
+            longitude=float(venue_info.get("longitude", "0.0")),
         )
         try:
             old_race = Race.get(spartan_id=overall_event["id"])
@@ -122,12 +129,14 @@ def main():
                     name=e["name"],
                     race_id=e["race_id"],
                     start_date=start_date,
-                    venue_name=overall_event["venue"]["name"],
+                    venue_name=venue_info["name"],
                 )
             except KeyError as ke:
                 # Item #1 overall_event["venue"]["name"] is missing - id 20 = Red deer
                 # Item #2 category_identifier missing for HH24HR
-                logging.exception(f"{ke}: event = {e} ; overall_event = {overall_event}")
+                logging.exception(
+                    f"{ke}: event = {e} ; overall_event = {overall_event}"
+                )
                 continue
 
             try:
