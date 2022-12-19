7aaeb478e2766e77e228f48f38908fc8b1243e3c
David Gadling <dave@toasterwaffles.com>
2022-12-18 20:17:35 -0800

    [gsheet_exporter] Better location info, actually posts
    This is a big one!
    1. Extract country, region, lat & long from the Race
    2. Order by start_date
    3. Actually publish data to gsheets!

--- gsheet_exporter.py
+++ gsheet_exporter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import itertools
 import os.path
 import re
 from datetime import date
-from typing import List, Dict
+from typing import List
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -70,7 +71,7 @@ class EventRow:
     ]
 
     def __init__(
-        self, race: Race, events: List[Event], normalize_race_name=False
+            self, race: Race, events: List[Event], normalize_race_name=False
     ) -> None:
         self.start_date = race.start_date
         self.name = race.name
@@ -78,8 +79,11 @@ class EventRow:
         self.event_link = (
             f"https://race.spartan.com/en/race/detail/{race.spartan_id}/overview"
         )
-        # Events for the same Race are never at different Venues
-        self.location = events[0].venue_name
+        self.venue_name = race.venue_name
+        self.country = race.country
+        self.region = race.region
+        self.latitude = race.latitude
+        self.longitude = race.longitude
 
         if normalize_race_name:
             self.name = self.normalize_race_name(race.name)
@@ -90,12 +94,13 @@ class EventRow:
             k
             for k in self.__dict__.keys()
             if k.startswith("__") is False
-            and k not in ["name", "start_date", "location", "event_id", "event_link"]
+               and k not in ["name", "start_date", "venue_name", "event_id",
+                             "event_link"]
         )
         attr_strs = [f"{attr}={getattr(self, attr)}" for attr in sorted(attrs)]
         return (
             f'EventRow(start_date="{self.start_date}", name="{self.name}", '
-            f'event_id={self.event_id}, location="{self.location}", '
+            f'event_id={self.event_id}, venue_name="{self.venue_name}", '
             f'{", ".join(attr_strs)}, event_link={self.event_link})'
         )
 
@@ -132,6 +137,73 @@ class EventRow:
 
         for category in event_categories:
             setattr(self, self.EVENT_CATEGORY_REPLACEMENTS[category], True)
+
+    RACE_ORDER = [
+        "kids",
+        "stadion",
+        "sprint",
+        "super",
+        "beast",
+        "ultra",
+        "city",
+        "hh_4",
+        "hh_12",
+        "trail_10k",
+        "trail_21k",
+        "trail_50k",
+        "trail_100k",
+    ]
+
+    def meta_to_sheet_row(self):
+        return [
+            self.start_date.strftime("%Y-%m-%d"),
+            f'=HYPERLINK("{self.event_link}", "{self.name}")',
+            self.venue_name,
+            self.country,
+            self.region,
+        ]
+
+    def date_and_location_row(self):
+        return [
+            self.start_date.strftime("%Y-%m-%d"),
+            f'=HYPERLINK("{self.event_link}", "{self.name}")',
+        ]
+
+    def country_region_row(self):
+        return [
+            self.venue_name,
+            self.country,
+            self.region,
+        ]
+
+    @staticmethod
+    def _generate_update_cells_request(row_index: int, column_index: int):
+        return {
+            "updateCells": {
+                "rows": [
+                    {
+                        "values": [
+                            {
+                                "dataValidation": {"condition": {"type": "BOOLEAN"}},
+                            }
+                        ],
+                    }
+                ],
+                "fields": "dataValidation",
+                "start": {
+                    "sheetId": 0,
+                    "rowIndex": row_index,
+                    "columnIndex": column_index,
+                },
+            }
+        }
+
+    def races_to_sheet_row(self, row_index: int):
+        return [
+            self._generate_update_cells_request(row_index, 2 + idx)
+            for idx, field in enumerate(self.RACE_ORDER)
+            if getattr(self, field)
+        ]
 
 
 def create(title, creds):
@@ -190,7 +262,7 @@ def get_event_rows(starting_on_or_after: date) -> List[EventRow]:
     races = (
         Race.select()
         .where(Race.start_date >= starting_on_or_after)
-        .order_by(Race.spartan_id)
+        .order_by(Race.start_date)
     )
 
     return [
@@ -199,14 +271,75 @@ def get_event_rows(starting_on_or_after: date) -> List[EventRow]:
     ]
 
 
+def set_location_info(creds, sheet_id, events: List[EventRow]) -> None:
+    """
+    Sets the event date, name, and location info (venue/country/region)
+    """
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        range_name = f"A4:B{len(events) + 3}"
+        body = {"values": [event_row.date_and_location_row() for event_row in events]}
+        (
+            service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId=sheet_id,
+                range=range_name,
+                valueInputOption="USER_ENTERED",
+                body=body,
+            )
+            .execute()
+        )
+        range_name = f"P4:R{len(events) + 3}"
+        body = {"values": [event_row.country_region_row() for event_row in events]}
+        (
+            service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId=sheet_id,
+                range=range_name,
+                valueInputOption="USER_ENTERED",
+                body=body,
+            )
+            .execute()
+        )
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+
+def set_race_info(creds, sheet_id, events: List[EventRow]) -> None:
+    all_requests = [
+        event.races_to_sheet_row(3 + idx) for idx, event in enumerate(events)
+    ]
+    flat_requests = list(itertools.chain(*all_requests))
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        (
+            service.spreadsheets()
+            .batchUpdate(
+                body={"requests": flat_requests},
+                spreadsheetId=sheet_id,
+            )
+            .execute()
+        )
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+
+def update_sheet(creds, sheet_id, events: List[EventRow]) -> None:
+    print("Setting location info")
+    set_location_info(creds, sheet_id, events)
+    print("Race specific info")
+    set_race_info(creds, sheet_id, events)
+
+
 def main():
     event_rows = get_event_rows(starting_on_or_after=date(2023, 1, 1))
     print(f"Found {len(event_rows)} events")
-    for event_row in event_rows:
-        print(event_row)
 
-    # creds = handle_creds()
-    # sheet_id = "15Tx_2Kqyg2Ir_HIkLPlzibfA9PmA_lkiazRLmPxMRiU"
+    update_sheet(
+        handle_creds(), "15Tx_2Kqyg2Ir_HIkLPlzibfA9PmA_lkiazRLmPxMRiU", event_rows
+    )
 
 
 if __name__ == "__main__":
